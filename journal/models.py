68b6122e3ef69766a0c855d9da839be9a89a9c63
lorenzo <adducilorenzo@hotmail.it>
2023-12-10 20:56:09 +0100

    model improvements

--- journal/models.py
+++ journal/models.py
@@ -1,8 +1,10 @@
 import datetime
 from dataclasses import dataclass
+from typeguard import typechecked
 from valid8 import validate
 
 
+@typechecked
 @dataclass(frozen=True)
 class Article:
     article_id: int
@@ -13,6 +15,18 @@ class Article:
     title: str
     body: str
     likes: list[str]
+
+    def __post_init__(self):
+        validate('body_length', self.body, max_len=1200)
+        validate('title-length', self.title, max_len=60)
+        validate('topic_length', self.topic, max_len=20)
+
+
+
+
+
+
+
 
 
 
--- tests/journal/test_domain.py
+++ tests/journal/test_domain.py
@@ -1,9 +1,96 @@
+import pytest
+from valid8 import ValidationError
+
 from journal.models import Article
-import datetime
+from datetime import datetime
 
 
-def test_article_format():
-    article = Article(1234, 2345, datetime.datetime(day=300, month=2, year=2023),
-                      datetime.datetime(day=300, month=2, year=2023),
-                      'asdfdsasdf', 'sport', 'rsgsegrsg', ['franco', 'giovanni'])
-    assert article is not None
+def test_article_creation():
+    article_id = 1
+    author_id = 101
+    created_at = datetime.now()
+    updated_at = datetime.now()
+    topic = "Technology"
+    title = "Introduction to Python"
+    body = "Python is a versatile programming language..."
+    likes = ["user1", "user2"]
+
+    article = Article(
+        article_id=article_id,
+        author_id=author_id,
+        created_at=created_at,
+        updated_at=updated_at,
+        topic=topic,
+        title=title,
+        body=body,
+        likes=likes
+    )
+
+
+def test_article_body_length():
+    article_id = 1
+    author_id = 101
+    created_at = datetime.now()
+    updated_at = datetime.now()
+    topic = "Technology"
+    title = "Introduction to Python"
+    body = "Python" * 2000
+    likes = ["user1", "user2"]
+
+    with pytest.raises(ValidationError):
+        article = Article(
+            article_id=article_id,
+            author_id=author_id,
+            created_at=created_at,
+            updated_at=updated_at,
+            topic=topic,
+            title=title,
+            body=body,
+            likes=likes
+        )
+
+
+def test_article_topic_length():
+    article_id = 1
+    author_id = 101
+    created_at = datetime.now()
+    updated_at = datetime.now()
+    topic = "Technology" * 10
+    title = "Introduction to Python"
+    body = "Python ....."
+    likes = ["user1", "user2"]
+
+    with pytest.raises(ValidationError):
+        article = Article(
+            article_id=article_id,
+            author_id=author_id,
+            created_at=created_at,
+            updated_at=updated_at,
+            topic=topic,
+            title=title,
+            body=body,
+            likes=likes
+        )
+
+
+def test_article_title_length():
+    article_id = 1
+    author_id = 101
+    created_at = datetime.now()
+    updated_at = datetime.now()
+    topic = "Technology"
+    title = "Introduction to Python" * 100
+    body = "Python ....."
+    likes = ["user1", "user2"]
+
+    with pytest.raises(ValidationError):
+        article = Article(
+            article_id=article_id,
+            author_id=author_id,
+            created_at=created_at,
+            updated_at=updated_at,
+            topic=topic,
+            title=title,
+            body=body,
+            likes=likes
+        )
