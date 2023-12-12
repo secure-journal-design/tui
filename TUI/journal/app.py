77dfe2aa8678b50bad21d8c952f569edf196b15f
lorenzo <adducilorenzo@hotmail.it>
2023-12-12 19:58:11 +0100

    App improvements

--- TUI/journal/app.py
+++ TUI/journal/app.py
@@ -7,17 +7,32 @@ from TUI.journal.models import Journal, Subheading, Body, Title, Topic, Article
 class App:
     def __init__(self):
         self.__menu = Menu.Builder(Description('Secure Journal Deisgn'), auto_select=lambda: self.__printArticles()) \
-            .with_entry(Entry.create('1', 'Add Journal')) \
+            .with_entry(Entry.create('1', 'Add article')) \
+            .with_entry(Entry.create('2', 'Print article')) \
+            .with_entry(Entry.create('3', 'Search by topic')) \
+            .with_entry(Entry.create('4', 'Sort by newest')) \
+            .with_entry(Entry.create('5', 'Sort by oldest')) \
+            .with_entry(Entry.create('6', 'Sort by like')) \
             .with_entry(Entry.create('0', 'Exit', on_selected=lambda: print("Bye!"), is_exit=True)) \
             .build()
         self.__journal = Journal()
-        test_article = Article(3, 20, datetime.fromtimestamp(1702390272), datetime.fromtimestamp(1702390272), Topic('Computer science'), Title('answer set programming (ASP)'), Body(                "L'answer set programming (ASP) è una forma di programmazione logica di tipo dichiarativo utilizzato per problemi di ricerca complessi (in primis NP-difficili), basata sulla semantica del modello stabile"), 120, Subheading('Answer Set Programming: advanced development'))
+        test_article = Article(3, 20, datetime.fromtimestamp(1702390272), datetime.fromtimestamp(1702390272),
+                               Topic('Computer science'), Title('answer set programming (ASP)'), Body(
+                "L'answer set programming (ASP) è una forma di programmazione logica di tipo dichiarativo utilizzato "
+                "per problemi di ricerca complessi (in primis NP-difficili), basata sulla semantica del modello "
+                "stabile"), 120, Subheading('Answer Set Programming: advanced development'))
         self.__journal.add_article(test_article)
 
     def __printArticles(self) -> None:
+        print_sep = lambda: print('-' * 150)
+        print_sep()
+        fmt = '%3s %-35.35s %-30.30s %-40.35s %-6s'
+        print(fmt % ('#', 'TITLE', 'TOPIC', 'BODY', 'LIKE'))
+        print_sep()
         for index in range(self.__journal.articles()):
             article = self.__journal.article(index)
-            print(article.article_id, article.topic)
+            print(fmt % (index + 1, article.title, article.topic, article.body, article.likes))
+        print_sep()
 
     def run(self):
         self.__menu.run()
